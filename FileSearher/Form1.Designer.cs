c17e7458902b80a99da1bfc300c2b06da9c583b1
loudger7 <Franticman717@gmail.com>
2019-12-17 14:25:35 +0300

    first version without broking thread

---
+++ FileSearher/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace FileSearher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.Start_Pause = new System.Windows.Forms.Button();
+            this.path_box = new System.Windows.Forms.TextBox();
+            this.filename_box = new System.Windows.Forms.TextBox();
+            this.content_box = new System.Windows.Forms.TextBox();
+            this.path_label = new System.Windows.Forms.Label();
+            this.filename_label = new System.Windows.Forms.Label();
+            this.content_label = new System.Windows.Forms.Label();
+            this.path_butt = new System.Windows.Forms.Button();
+            this.dropping_bt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(422, 568);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Start_Pause
+            // 
+            this.Start_Pause.Location = new System.Drawing.Point(501, 277);
+            this.Start_Pause.Name = "Start_Pause";
+            this.Start_Pause.Size = new System.Drawing.Size(155, 55);
+            this.Start_Pause.TabIndex = 1;
+            this.Start_Pause.Text = "Пуск";
+            this.Start_Pause.UseVisualStyleBackColor = true;
+            this.Start_Pause.Click += new System.EventHandler(this.Start_Pause_Click);
+            // 
+            // path_box
+            // 
+            this.path_box.Location = new System.Drawing.Point(751, 53);
+            this.path_box.Name = "path_box";
+            this.path_box.Size = new System.Drawing.Size(140, 20);
+            this.path_box.TabIndex = 2;
+            // 
+            // filename_box
+            // 
+            this.filename_box.Location = new System.Drawing.Point(751, 94);
+            this.filename_box.Name = "filename_box";
+            this.filename_box.Size = new System.Drawing.Size(140, 20);
+            this.filename_box.TabIndex = 3;
+            // 
+            // content_box
+            // 
+            this.content_box.Location = new System.Drawing.Point(751, 136);
+            this.content_box.Name = "content_box";
+            this.content_box.Size = new System.Drawing.Size(140, 20);
+            this.content_box.TabIndex = 4;
+            // 
+            // path_label
+            // 
+            this.path_label.AutoSize = true;
+            this.path_label.Location = new System.Drawing.Point(699, 56);
+            this.path_label.Name = "path_label";
+            this.path_label.Size = new System.Drawing.Size(29, 13);
+            this.path_label.TabIndex = 5;
+            this.path_label.Text = "Path";
+            // 
+            // filename_label
+            // 
+            this.filename_label.AutoSize = true;
+            this.filename_label.Location = new System.Drawing.Point(699, 97);
+            this.filename_label.Name = "filename_label";
+            this.filename_label.Size = new System.Drawing.Size(46, 13);
+            this.filename_label.TabIndex = 6;
+            this.filename_label.Text = "filename";
+            // 
+            // content_label
+            // 
+            this.content_label.AutoSize = true;
+            this.content_label.Location = new System.Drawing.Point(699, 139);
+            this.content_label.Name = "content_label";
+            this.content_label.Size = new System.Drawing.Size(43, 13);
+            this.content_label.TabIndex = 7;
+            this.content_label.Text = "content";
+            // 
+            // path_butt
+            // 
+            this.path_butt.Location = new System.Drawing.Point(610, 45);
+            this.path_butt.Name = "path_butt";
+            this.path_butt.Size = new System.Drawing.Size(75, 23);
+            this.path_butt.TabIndex = 8;
+            this.path_butt.Text = "Browse";
+            this.path_butt.UseVisualStyleBackColor = true;
+            this.path_butt.Click += new System.EventHandler(this.path_butt_Click);
+            // 
+            // dropping_bt
+            // 
+            this.dropping_bt.Location = new System.Drawing.Point(711, 277);
+            this.dropping_bt.Name = "dropping_bt";
+            this.dropping_bt.Size = new System.Drawing.Size(155, 55);
+            this.dropping_bt.TabIndex = 9;
+            this.dropping_bt.Text = "Сброс";
+            this.dropping_bt.UseVisualStyleBackColor = true;
+            this.dropping_bt.Click += new System.EventHandler(this.dropping_bt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(449, 567);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 592);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dropping_bt);
+            this.Controls.Add(this.path_butt);
+            this.Controls.Add(this.content_label);
+            this.Controls.Add(this.filename_label);
+            this.Controls.Add(this.path_label);
+            this.Controls.Add(this.content_box);
+            this.Controls.Add(this.filename_box);
+            this.Controls.Add(this.path_box);
+            this.Controls.Add(this.Start_Pause);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button Start_Pause;
+        private System.Windows.Forms.TextBox path_box;
+        private System.Windows.Forms.TextBox filename_box;
+        private System.Windows.Forms.TextBox content_box;
+        private System.Windows.Forms.Label path_label;
+        private System.Windows.Forms.Label filename_label;
+        private System.Windows.Forms.Label content_label;
+        private System.Windows.Forms.Button path_butt;
+        private System.Windows.Forms.Button dropping_bt;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileSearher/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FileSearher
+{
+    public partial class Form1 : Form
+    {
+        private static bool is_pause = false; // если нажата пауза, то true
+        private static bool is_work = false; // если поиск не работает или нажат "сброс", то false
+        static ManualResetEvent _event; // событие для остановки search_files потока
+        Thread search_files; // поиск и вывод файлов в дереве
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void Start_Pause_Click(object sender, EventArgs e)
+        {
+            search_files = new Thread(new ParameterizedThreadStart(AuxiliaryCreateDirectoryNode));
+
+            if (!is_work)
+            {
+                Start_Pause.Text = "Пауза";
+                _event = new ManualResetEvent(true);
+                is_work = true;
+
+                // ограничиваю возможность изменения формы
+                path_label.Enabled = false;
+                filename_label.Enabled = false;
+                content_label.Enabled = false;
+
+                string path = path_box.Text;
+                string filename_pattern = filename_box.Text;
+                string contain_str = content_box.Text;
+
+                var rootDirectoryInfo = new DirectoryInfo(path);
+                if (filename_pattern == "")
+                    filename_pattern = "*";
+                if (contain_str == "")
+                    contain_str = null;
+
+                // использую вспомогательный класс, чтобы перекинуть все атрибуты в поток
+                AuxiliaryClassObject auxiliary_class_object = new AuxiliaryClassObject();
+                auxiliary_class_object.directoryInfo = rootDirectoryInfo;
+                auxiliary_class_object.filename_pattern = filename_pattern;
+                auxiliary_class_object.contain_str = contain_str;
+
+                // запускаю поток
+                search_files.Start(auxiliary_class_object);
+            }
+            else if(is_work && !is_pause)
+            {
+                Start_Pause.Text = "Пуск";
+                is_pause = true;
+                // событие остановки работы потока
+                _event.Reset();
+            }
+            else if (is_work && is_pause)
+            {
+                Start_Pause.Text = "Пауза";
+                is_pause = false;
+                // событие продолжения работы потока
+                _event.Set();
+            }
+
+        }
+
+        private void AuxiliaryCreateDirectoryNode(object auxiliary_class_object)
+        {
+            // вспомогательный метод,чтобы перебросить все атрибуты в метод CreateDirectoryNode из auxiliary_class_object
+            AuxiliaryClassObject x = (AuxiliaryClassObject)auxiliary_class_object;
+            CreateDirectoryNode(x.directoryInfo, x.filename_pattern, x.contain_str);
+        }
+
+        private void CreateDirectoryNode(DirectoryInfo directoryInfo, string filename_pattern, string contain_str)
+        {
+            
+            _event.WaitOne();
+            
+            // проходит по всем каталогам
+            foreach (var directory in directoryInfo.GetDirectories())
+                try
+                {
+                    // проходит по всем каталогом внутри каталогов
+                    CreateDirectoryNode(directory, filename_pattern, contain_str);
+                }
+                catch (System.UnauthorizedAccessException expt)
+                {
+                    // обработка исключения на случай, если не хватает прав, чтобы зайти в каталог
+                }
+            // проходит по всем файлам внутри каталога
+            foreach (var file in directoryInfo.GetFiles(filename_pattern))
+            {
+
+                try
+                {
+                    // нужно ли читать файл?
+                    if (contain_str != null)
+                    {
+                        // читает файл, находит совпадения
+                        if (File.ReadAllText(file.FullName).Contains(contain_str))
+                        {
+                            CreateNode(file.FullName.Split('\\'), 0);
+                        }
+                    }
+                    else
+                    {
+                        CreateNode(file.FullName.Split('\\'), 0);
+                    }
+                    
+                }
+                catch (System.IO.IOException excpt)
+                {
+                    // обработка исключения на случай, если не хватает прав, чтобы прочитать файл
+                }
+            }
+        }
+
+        private void CreateNode(string[] file_path, int deep, TreeNodeCollection cur_node = null)
+        {
+            // метод, которое передаётся основному потоку на добавления узла в дереве.
+            Action action = () => cur_node.Add(file_path[deep], file_path[deep]);
+
+            // проверка, последнюю ли часть file_path мы читаем (чтобы не выйти за пределы массива)
+            if (deep != file_path.Length-1)
+            {
+                // если данный узел не известен, то вписывать в корень дерева
+                if (cur_node == null)
+                    cur_node = treeView1.Nodes;
+
+                int i = cur_node.IndexOfKey(file_path[deep]);
+                // если нет такого узла, то i = -1
+                if (i >= 0)
+                {
+                    // создаём следующий узел
+                    deep++;
+                    CreateNode(file_path, deep, cur_node[i].Nodes);
+                }
+                else
+                {
+                    if (InvokeRequired)
+                    {
+                        // передаёт основному потоку метод, для добавления нового узла
+                        Invoke(action);
+                    }
+                    else
+                    {
+                        action();
+                    }
+                    deep++;
+                    CreateNode(file_path, deep, cur_node[cur_node.Count - 1].Nodes);
+                }
+            }
+            else
+            {
+                if (InvokeRequired)
+                {
+                    Invoke(action);
+                }
+                else
+                {
+                    action();
+                }
+            }
+        }
+
+        private void path_butt_Click(object sender, EventArgs e)
+        {
+            // открывает проводник для выбора начального пути
+            using(FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                    path_box.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void dropping_bt_Click(object sender, EventArgs e)
+        {
+            // должен прерывать процесс
+            search_files.Abort();
+            // очистка всей памяти в 3 этапа
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            treeView1.Nodes.Clear();
+            Start_Pause.Text = "Пуск";
+            is_work = false;
+        }
+    }
+
+    public class AuxiliaryClassObject
+    {
+        public DirectoryInfo directoryInfo;
+        public string filename_pattern;
+        public string contain_str;
+    }
+}
+
